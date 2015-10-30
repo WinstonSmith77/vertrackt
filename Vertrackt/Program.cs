6446f4e37e189dde8d9c155f12396fc7b21347c7
Matthias Henning <Matze_GitHub@gmx.de>
2015-10-30 18:53:38 +0100

    Erstcommit

---
+++ Vertrackt/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vertrackt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

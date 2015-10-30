9d9bf2cb7a03c709626c7cf53496c258134676cf
Matthias Henning <Matze_GitHub@gmx.de>
2015-10-30 19:05:56 +0100

    Feierabend

---
+++ Vertrackt/Geometry/Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vertrackt.Geometry
+{
+    public class Point
+    {
+        private readonly int _xPos;
+        private readonly int _yPos;
+        private int _xSpeed;
+        private int _ySpeed;
+
+        public Point(int xPos, int yPos)
+        {
+            _xPos = xPos;
+            _yPos = yPos;
+        }
+
+        public Point Iterate(int xAcc, int yAcc)
+        {
+            var newX = _xPos + _xSpeed;
+            var newY = _yPos + _ySpeed;
+
+            return new Point(newX, newY);
+        }
+    }
+}
